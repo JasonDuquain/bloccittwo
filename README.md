a95b547a5dd02cf64e8d10af0d0a2e073c8e0052
jasonduquain <jduquain@gmail.com>
2019-04-10 21:51:35 -0400

    post-resource-part-two_complete

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bloccit
+
+Bloccit is a reddit-style app that allows users to post, comment, and vote on each others posts. It was built using an MVC pattern and some features include authentication, authorization, creating topics, posts, comments, favoriting, and voting.
+
+## Getting Started
+
+* Clone
+* Open terminal and change the directory to the bloccit folder
+* run npm install
+* run npm start
+
+## Built With
+
+* [Node] (https://nodejs.org/en/) - An asynchronous event driven JavaScript runtime
+* [Express](https://expressjs.com/) - The web framework used
+* [EJS](https://ejs.co/) - Embedded JavaScript templating
+* [Sequelize](https://sequelize.readthedocs.io/en/v3/) - Object Relational Mapper for Node.js projects
+
+
+## Authors
+
+* **Jason Duquain** - [JasonDuquain](https://https://github.com/JasonDuquain)
+
+
+
---
+++ src/db/models/post.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    
+  var Post = sequelize.define('Post', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+      topicId: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+      }
+  }, {});
+  
+    //We associate the Post model with the Topic model using a one-to-many association. Specifically, we say that a Post object belongs to a Topic object. We pass an options object specifying configuration options including the foreign key to use and what to do when we delete the parent of a post object. Sequelize creates methods based on the association we describe. In this case, it creates Post model instance methods like setTopic and getTopic which allow us to set and get the topic associated with a post instance. 
+  Post.associate = function(models) {
+    // associations can be defined here
+      Post.belongsTo(models.Topic, { //models is plural!!
+          foreignKey: "topicId",
+          onDelete: "CASCADE"
+      });
+  };
+  return Post;
+    
+};
